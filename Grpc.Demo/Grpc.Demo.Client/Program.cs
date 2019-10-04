7b37325cae0ae33e311a132a1a889f953c28050b
Shengjie Yan <ysjshengjie@live.cn>
2019-10-04 10:26:30 +0800

    learn how to use duplex stream with grpc

--- Grpc.Demo/Grpc.Demo.Client/Program.cs
+++ Grpc.Demo/Grpc.Demo.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using Grpc.Core;
 using System.Threading.Tasks;
 using Google.Protobuf.WellKnownTypes;
@@ -9,7 +10,14 @@ namespace Grpc.Demo.Client
 {
     class Program
     {
-        async static Task Main(string[] args)
+        static async Task Main(string[] args)
+        {
+            await BathTheCats();
+            //            await HelloGrpc();
+            Console.ReadKey();
+        }
+
+        private static async Task HelloGrpc()
         {
             var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new Greeter.GreeterClient(channel);
@@ -18,10 +26,53 @@ namespace Grpc.Demo.Client
             Console.WriteLine("Greeter Return: " + reply.Message);
 
             var catClient = new LuCat.LuCatClient(channel);
-            
+
             var catReply = await catClient.SuckingCatAsync(new Empty());
-            Console.WriteLine("调用撸猫服务："+ catReply.Message);
-            Console.ReadKey();
+            Console.WriteLine("调用撸猫服务：" + catReply.Message);
+        }
+
+        private static async Task BathTheCats()
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var catClient = new LuCat.LuCatClient(channel);
+            var catCount = await catClient.CountAsync(new Empty());
+
+            Console.WriteLine($"猫：{catCount}");
+
+            var cts = new CancellationTokenSource();
+
+            cts.CancelAfter(TimeSpan.FromSeconds(2.5));
+
+            var bathCat = catClient.BathTheCat(cancellationToken: cts.Token);
+
+            var bathCatRespTask = Task.Run(async () =>
+            {
+                try
+                {
+                    await foreach (var resp in bathCat.ResponseStream.ReadAllAsync(cancellationToken: cts.Token))
+                    {
+                        Console.WriteLine(resp.Message);
+                    }
+                }
+                catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+                {
+                    Console.WriteLine("Stream cancelled.");
+                }
+            });
+
+            var rand = new Random(DateTime.Now.Millisecond);
+            for (int i = 0; i < 10; i++)
+            {
+                var id = rand.Next(0, catCount.Count);
+                await bathCat.RequestStream.WriteAsync(new BathTheCatReq() { Id = id });
+                Console.WriteLine($"{i}. No.{id} 已进行排队洗澡！");
+            }
+
+            await bathCat.RequestStream.CompleteAsync();
+
+            await bathCatRespTask;
+
+            Console.WriteLine();
         }
     }
 }
--- Grpc.Demo/Grpc.Demo.Server/Services/LuCatService.cs
+++ Grpc.Demo/Grpc.Demo.Server/Services/LuCatService.cs
@@ -10,8 +10,39 @@ namespace Grpc.Demo.Server
 {
     public class LuCatService : LuCat.LuCatBase
     {
+        private readonly ILogger<LuCatService> _logger;
         private static readonly List<string> Cats = new List<string>() { "”¢∂Ã“¯Ω•≤„", "”¢∂ÃΩΩ•≤„", "√¿∂Ã", "¿∂√®", "¿Íª®√®", "ÈŸ√®" };
         private static readonly Random Rand = new Random(DateTime.Now.Millisecond);
+
+        public LuCatService(ILogger<LuCatService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task BathTheCat(IAsyncStreamReader<BathTheCatReq> requestStream, IServerStreamWriter<BathTheCatResp> responseStream, ServerCallContext context)
+        {
+            var bathQueue = new Queue<int>();
+            while (await requestStream.MoveNext())
+            {
+                bathQueue.Enqueue(requestStream.Current.Id);
+
+                _logger.LogInformation($"NO.{requestStream.Current.Id} Enqueue.");
+            }
+
+            while (!context.CancellationToken.IsCancellationRequested
+                   && bathQueue.TryDequeue(out var catId))
+            {
+                await responseStream.WriteAsync(new BathTheCatResp() { Message = $"No.{catId} {Cats[catId]} “—œ¥‘Ë£°" });
+                await Task.Delay(500);
+            }
+
+        }
+
+        public override Task<CountCatResult> Count(Empty request, ServerCallContext context)
+        {
+            return Task.FromResult(new CountCatResult() { Count = Cats.Count });
+        }
+
         public override Task<SuckingCatResult> SuckingCat(Empty request, ServerCallContext context)
         {
             return Task.FromResult(new SuckingCatResult()
@@ -19,5 +50,7 @@ namespace Grpc.Demo.Server
                 Message = $"ƒ˙Œ¸¡À“ª÷ª{Cats[Rand.Next(0, Cats.Count)]}"
             });
         }
+
     }
+
 }
