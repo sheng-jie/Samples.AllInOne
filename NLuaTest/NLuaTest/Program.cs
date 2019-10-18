21224f516217aef1b9d1a0ead773c82384d47109
Shengjie Yan <ysjshengjie@live.cn>
2019-10-18 10:21:47 +0800

    add nlua test

---
+++ NLuaTest/NLuaTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using NLua;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace NLuaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = Environment.Is64BitOperatingSystem;
+            IntPtr lib = IntPtr.Zero;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                var path = Path.Combine(AppContext.BaseDirectory, "lib", "mysql.dll");
+                lib = NativeLibrary.Load(path, Assembly.GetExecutingAssembly(), null);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                var path = Path.Combine(AppContext.BaseDirectory, "lib", "mysql.so");
+                lib = NativeLibrary.Load(path, Assembly.GetExecutingAssembly(), null);
+            }
+            IntPtr method = NativeLibrary.GetExport(lib, "luaopen_luasql_mysql");
+            KeraLua.LuaFunction function = Marshal.GetDelegateForFunctionPointer<KeraLua.LuaFunction>(method);
+            using (var lua = new Lua())
+            {
+                var b = function.GetMethodInfo();
+                KeraLua.Lua state = lua.State;
+                IntPtr ptr = lua.State.Handle;
+                state.RequireF("mysql", function, true);
+                var result = lua.DoFile("test.lua");
+            }
+        }
+    }
+}
---
+++ NLuaTest/NLuaTest/test.lua
@@ -0,0 +1,12 @@
+﻿require "luasql.mysql"
+
+print("connecting")
+
+env = luasql.mysql()
+
+conn = env:connect("dbname","user","pwd","IP address",3306)
+
+print("connected")
+
+conn:close() 
+env:close()  
