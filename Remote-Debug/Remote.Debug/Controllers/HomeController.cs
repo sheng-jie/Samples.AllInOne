f968d1f05297b457f5767849c10162522485c3d7
Shengjie Yan <ysjshengjie@live.cn>
2019-10-20 22:32:58 +0800

    add orleans demo! will update with a suitable example

--- Remote-Debug/Remote.Debug/Controllers/HomeController.cs
+++ Remote-Debug/Remote.Debug/Controllers/HomeController.cs
@@ -25,5 +25,7 @@ namespace Remote.Debug.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+    
     }
+
 }
