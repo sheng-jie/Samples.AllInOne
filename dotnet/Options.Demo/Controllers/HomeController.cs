570a6d09f06f03614e6ee0fd23840d3576e53a5a
Shengjie Yan <ysjshengjie@live.cn>
2019-10-30 19:00:47 +0800

    demo for using Complex configuration classes
    https://andrewlock.net/how-to-use-the-ioptions-pattern-for-configuration-in-asp-net-core-rc2/

---
+++ dotnet/Options.Demo/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Options.Demo.Models;
+
+namespace Options.Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IOptions<DeveloperOptions> _developerOptions;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(IOptions<DeveloperOptions> developerOptions,ILogger<HomeController> logger)
+        {
+            _developerOptions = developerOptions;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ dotnet/Options.Demo/DeveloperOptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Options.Demo
+{
+    public class DeveloperOptions
+    {
+        public Dictionary<string, WeixinOptions> Developer { get; set; }
+    }
+
+    public class WeixinOptions
+    {
+        public string WeixinAppId { get; set; }
+        public string WeixinAppSecret { get; set; }
+        public Templates Templates { get; set; }
+    }
+
+
+
+    public class Templates
+    {
+        public string BindingRequest { get; set; }
+        public string ApproveBindingRequest { get; set; }
+        public string OnlineNotice { get; set; }
+        public string WarningNotice { get; set; }
+    }
+}
