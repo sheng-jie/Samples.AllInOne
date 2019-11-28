96085d8ea3bdeea46af28761679e6c0aa749fb24
Shengjie Yan <ysjshengjie@live.cn>
2019-11-28 10:40:47 +0800

    add unifrom action filter

---
+++ dotnet/Webapi.Demo/UniformActionFilter.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace Webapi.Demo
+{
+    /// <summary>
+    /// 统一Action Filter
+    /// </summary>
+    public class UniformActionFilter : IAsyncActionFilter, IActionFilter
+    {
+        private readonly ILogger<UniformActionFilter> _logger;
+
+        readonly Stopwatch _watch = new Stopwatch();
+
+        public UniformActionFilter(ILogger<UniformActionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public  async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            _watch.Start();
+            if (context.ModelState.IsValid)
+            {
+                await next();
+            }
+            else
+            {context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            _watch.Stop();
+
+            _logger.LogInformation($"It takes {_watch.ElapsedMilliseconds}ms");
+
+
+        }
+    }
+}
