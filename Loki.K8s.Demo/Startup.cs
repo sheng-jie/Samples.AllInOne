2fdd8a3292a229867b0878c6af49a58b417291a8
Shengjie Yan <ysjshengjie@live.cn>
2020-07-28 07:17:36 +0800

    test di

--- Loki.K8s.Demo/Startup.cs
+++ Loki.K8s.Demo/Startup.cs
@@ -14,15 +14,16 @@ namespace Loki.K8s.Demo
 {
     public class Startup
     {
-       
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<ScopedService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,ILogger<Startup> logger)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
@@ -30,17 +31,41 @@ namespace Loki.K8s.Demo
             }
 
             app.UseRouting();
-            
+
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
                     logger.LogInformation("start handle request!");
-                    await context.Response.WriteAsync("Hello World!");
+                    var factory = context.RequestServices.GetRequiredService<IServiceScopeFactory>();
+                    using (var scope = factory.CreateScope())
+                    {
+                        var scopedSvc = scope.ServiceProvider.GetRequiredService<ScopedService>();
+                        var str = scopedSvc.GetStr();
+                        await context.Response.WriteAsync($"Hello World!{str}");
+                        //scopedSvc.Dispose();
+                    }
                     logger.LogInformation("end handle request!");
                 });
             });
         }
+    }
+
+    public class ScopedService : IDisposable
+    {
+        private readonly ILogger<ScopedService> logger;
+
+        public ScopedService(ILogger<ScopedService> logger)
+        {
+            logger.LogInformation($"{this.GetType()} has initialized!");
+            this.logger = logger;
+        }
+        public void Dispose()
+        {
+            logger.LogInformation($"{this.GetType()} is disposed!");
+        }
+
+        public string GetStr() => Guid.NewGuid().ToString();
     }
 }
